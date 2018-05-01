3131cdf63b6344711661574d602cebfea404813f
Aldo <aldo@playsure.co>
2018-05-01 09:05:22 +0700

    End of section 2

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Aldo on 30/04/18.
+//  Copyright © 2018 Ayu Adisti. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+    
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Aldo on 30/04/18.
+//  Copyright © 2018 Ayu Adisti. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        
+        
+        
+        
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
